0e109fa2c32b815271964ef2ad5b0a60d0b1d3c9
libw <libowen.dev@foxmail.com>
2022-11-11 14:38:03 +0800

    初始化项目

---
+++ src/main/java/com/wbllwa/WbllwaCommonSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.wbllwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbllwaCommonSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbllwaCommonSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbllwa/WbllwaCommonSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbllwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbllwaCommonSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
